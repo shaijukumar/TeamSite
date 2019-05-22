6af07b1b7231deda942523ceaf57ae78fc6f55e3
shaijukumar <shaijukumar1980@gmail.com>
2019-05-22 07:14:59 +0400

    Change2

---
+++ src/app/team-home-page/team-home-page.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-team-home-page',
+  templateUrl: './team-home-page.component.html',
+  styleUrls: ['./team-home-page.component.scss']
+})
+export class TeamHomePageComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
